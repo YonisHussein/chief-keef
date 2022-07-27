7d9a61734ddd8999f75540e09e8af3b81d7e399b
YonisHussein <yonismhussein@gmail.com>
2022-07-27 01:40:33 -0400

    updated search books and schemas

--- 02-Challenge/Develop/client/src/pages/SearchBooks.js
+++ 02-Challenge/Develop/client/src/pages/SearchBooks.js
@@ -69,10 +69,6 @@ const SearchBooks = () => {
     try {
       const response = await saveBook(bookToSave, token);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
